4c98102609dc18f1a412e42d34cf6fa36a728f58
Timothy Sanders <timsanders07@gmail.com>
2023-07-10 01:05:25 -0400

    add sidebar functionality and cart html

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,17 @@
 import { getElement } from './utils.js';
+
+const toggleNav = getElement('.toggle-nav');
+const sidebarOverlay = getElement('.sidebar-overlay');
+const closeBtn = getElement('.sidebar-close');
+
+// Event Listeners
+
+toggleNav.addEventListener('click', () => {
+    sidebarOverlay.classList.add('show');
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebarOverlay.classList.remove('show');
+})
+
+// We're going to import the entire file into index.js
