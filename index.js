afc1c08b7929309830d188db987c90933e98da50
Timothy Sanders <timsanders07@gmail.com>
2023-07-11 02:30:34 -0400

    fetch products from api

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ import fetchProducts from './src/fetchProducts.js';
 import { setupStore, store } from './src/store.js';
 import display from './src/displayProducts.js';
 import { getElement } from './src/utils.js';
+
+
+const init = async () => {
+    const products = await fetchProducts();
+    if(products){
+        // add products to the store
+        setupStore(products)
+        
+    }
+}
+
+window.addEventListener('DOMContentLoaded', init)
