d3a1d888652d6befa4db38a401d92990186f3915
Timothy Sanders <timsanders07@gmail.com>
2023-07-19 01:05:43 -0400

    bug fix

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -17,7 +17,7 @@ const setupCompanies = (store) => {
             } else {
                 newStore = store.filter(product => product.company === element.textContent)
             }
-            display(newStore, getElement('.products-container'))
+            display(newStore, getElement('.products-container'), true)
         }
     })
 };
--- src/filters/price.js
+++ src/filters/price.js
@@ -19,7 +19,7 @@ const setupPrice = (store) => {
         const value = parseInt(priceInput.value);
         priceValue.textContent = `Value : ${value}`;
         let newStore = store.filter(product => product.price / 100 <= value);
-        display(newStore, getElement('.products-container'));
+        display(newStore, getElement('.products-container'), true);
         if(newStore.length < 1){
             const product = getElement('.products-container');
             product.innerHTML = `<h3 class='filter-error'>Sorry no products match your search.</h3>`
